eb3a49756e32793612f937daf3e90d50965b18ac
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-06-03 15:40:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.00af0fbea6a71ce3bd35d867513d025c.js"
 );
 
 self.addEventListener('message', (event) => {
